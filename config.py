0c666445e1f47d84a29cf684b55d632e7119e7d2
kubometr <kubometr@email.cz>
2025-07-01 01:13:16 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+API_ID = 26239288
+API_HASH = "ff1c94466d5e504193b35b8843ed316e"
+SESSION_NAME = "my_session"
+
+# Telegram
+SUPERGROUP_ID = -1002433338673
+TOPIC_ID = 8013
+
+# Пути
+BASE_DIR = "/home/kubometr/YoutubeGrab"
+LINKS_PATH = f"{BASE_DIR}/links.txt"
+TEMP_DIR = f"{BASE_DIR}/temp_videos"
